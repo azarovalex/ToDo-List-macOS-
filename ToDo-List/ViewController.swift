36dda68ce4b2ebc4ecf8ccb4a510f8c1ba33fb14
azarovalex <alex@azarov.by>
2018-05-14 08:51:17 +0300

    Initial Commit

---
+++ ToDo-List/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ToDo-List
+//
+//  Created by Alex Azarov on 5/14/18.
+//  Copyright © 2018 Alex Azarov. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
